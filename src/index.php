8ba2a0524f8982b542581aba692ec0cbe9b70dbe
Ab-dell <hakemabdel59@gmail.com>
2024-02-23 15:07:30 +0100

    remove the blank everywhere

--- src/index.php
+++ src/index.php
@@ -1,8 +1,8 @@
 <?php
 
-echo "Hello from your first test done in CI and github actions ";
+echo "Hello from your first test done in CI and github actions";
 echo "<br>";
-echo "Please answer the following questions ";
+echo "Please answer the following questions";
 echo "<br>";
 echo "question1";
 echo "<br>";
